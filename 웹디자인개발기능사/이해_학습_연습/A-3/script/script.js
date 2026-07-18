4985cf6928ce1e5aa116c0758a1795755ad638e9
evbbnhj84-cpu <evbbnhj84@gmail.com>
2026-07-18 17:59:05 +0900

    연습

--- 웹디자인개발기능사/이해_학습_연습/A-3/script/script.js
+++ 웹디자인개발기능사/이해_학습_연습/A-3/script/script.js
@@ -1,30 +1,27 @@
 $(function() {
     /* 메뉴 슬라이드 */
-    $(".main-menu").on("mouseover", function() {
-        $(".sub-menu").stop().slideDown()
+    $(".main-menu li").on("mouseover", function() {
+        $(this).find(".sub-menu").stop().slideDown()
     })
-    $(".main-menu").on("mouseout", function() {
-        $(".sub-menu").stop().slideUp()
+    $(".main-menu li").on("mouseout", function() {
+        $(this).find(".sub-menu").stop().slideUp()
     })
 
     /* 이미지 슬라이드 */
     // 밀리초 (ms) = 1/1000초 = 0.001초 --> 1000ms = 1초
+    let index = 0;   // 0 1 2  0 1 2  0 1 2
     const speed = 500; // 이미지가 전환되는 시간 (0.5초)
     const time = 2000; // 이미지가 보여지는 시간 (2초)
     const $slide = $(".slide");
-    const $container = $(".slide-container");
     const size = $slide.height();  // 높이
     const count = $slide.length;  // 개수
-    $container.height(size * count) // 300*3 = 900
+    $slide.not(":first").hide()
     // 타이머 함수
     setInterval(() => {
-        // 세로 슬라이드 동작
-        $container.animate({
-            top: -size
-        }, speed, function() {
-            $container.css('top', '0');
-            $container.append($(".slide").first());
-        })
+        // 페이드 슬라이드 동작
+        index = (index + 1) % count // 1 2 0 (반복)
+        $slide.fadeOut(speed)
+        $slide.eq(index).fadeIn(speed)
     }, time);
 
     /* 탭(공지사항+갤러리) */
@@ -45,8 +42,8 @@ $(function() {
     /* 레이어 팝업 */
     $(".btn-modal").on("click", function() {
         $("#modal").show()
-    });
+    })
     $(".close").on("click", function() {
         $("#modal").hide()
-    });
+    })
 })
