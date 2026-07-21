ac89c64891db9a6288aa50b157da8146695c81b9
evbbnhj84-cpu <evbbnhj84@gmail.com>
2026-07-21 22:35:09 +0900

    연습

---
+++ 웹디자인개발기능사/따라치기_연습/C/JS/C-3.js
@@ -0,0 +1,43 @@
+
+$(function() {
+    $(".main-menu li").on("mouseover", function(){
+        $(this).find(".sub-menu").stop().slideDown()
+    })
+    $(".main-menu li").on("mouseout", function(){
+        $(this).find(".sub-menu").stop().slideUp()
+    })
+
+
+
+
+
+    const speed = 500;
+    const time = 3000;
+    const $slide = $(".slide")
+    const $container = $(".slide-container")
+    const size = $slide.width();
+    const count = $slide.length;
+    $container.width(size * count)
+    
+    setInterval(function() {
+        $container.animate({
+            left: -size
+        }, speed, function() {
+            $container.css('left','0')
+            $container.append($(".slide").first());
+        } );
+    }, time);
+
+
+
+
+
+$(".btn-modal").on("click", function() {
+        $("#modal").show();
+    });
+
+    $(".close").on("click", function() {
+        $("#modal").hide();
+    });
+
+})
